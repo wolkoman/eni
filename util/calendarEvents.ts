4d6407f3a97db37d104813f5bcf7bebe3f3acec5
Manuel Wolkowitsch <superwolko@gmail.com>
2021-09-07 16:11:10 +0200

    :recycle: Refactor calendar events utility

--- util/calendarEvents.ts
+++ util/calendarEvents.ts
@@ -23,56 +23,64 @@ export interface CalendarEvent {
 
 export type CalendarEvents = Record<string, CalendarEvent[]>;
 
-export async function getEvents(props: { public: boolean }): Promise<CalendarEvents> {
+async function getEventsFromCalendar(calendarId: string, calendarName: string, isPublic: boolean): Promise<CalendarEvent[]> {
+  const oauth2Client = await getCachedGoogleAuthClient();
+  const calendar = google.calendar('v3');
+  const todayDate = new Date();
+  todayDate.setHours(0);
+  const today = todayDate.getTime();
+  const eventsList = await calendar.events.list({
+    calendarId,
+    auth: oauth2Client,
+    timeMin: new Date(today).toISOString(),
+    timeMax: new Date(today + 3600000 * 24 * 30 * (isPublic ? 1 : 6)).toISOString(),
+    singleEvents: true,
+    timeZone: 'Europa/Vienna',
+    orderBy: 'startTime'
+  });
+  return eventsList.data.items!.map(event => ({
+    id: event.id,
+    summary: event.summary,
+    description: event.description ?? null,
+    date: (event.start?.date ?? event.start?.dateTime ?? '').substr(0, 10),
+    start: event.start,
+    end: event.end,
+    calendar: calendarName,
+    visibility: event.visibility ?? 'public',
+    wholeday: !!event.start?.date,
+  } as CalendarEvent)).filter(event => event.summary)
+    .filter(event => !isPublic || event?.visibility === 'public')
+    .map(event => !isPublic ? event : ({
+      ...event,
+      summary: event.summary.replace(/\[.*?]/g, ''),
+      description: event.description?.replace(/\[.*?]/g, ''),
+    }));
+}
 
+let oauth2Client: any;
+
+async function getCachedGoogleAuthClient() {
+  if (oauth2Client) return oauth2Client;
   const configResponse = await cockpit.collectionGet('internal-data', {filter: {_id: '60d2474f6264631a2e00035c'}});
   const config = configResponse.entries[0].data;
-
-  const oauth2Client = new google.auth.OAuth2(
+  oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_KEY,
   );
   oauth2Client.setCredentials(config);
+  return oauth2Client;
+}
 
+export async function getEvents(props: { public: boolean }): Promise<CalendarEvents> {
 
-  const calendar = google.calendar('v3');
   const getTimeOfEvent = (event: any) => new Date(event!.start?.date ?? event!.start?.dateTime!).getTime();
-  const todayDate = new Date();
-  todayDate.setHours(0);
-  const today = todayDate.getTime();
   const allEvents = (await Promise.all(
-    Object.entries(calendarIds)
-      .map(async ([name, calendarId]) =>
-        (await calendar.events.list({
-          calendarId,
-          auth: oauth2Client,
-          timeMin: new Date(today).toISOString(),
-          timeMax: new Date(today + 3600000 * 24 * 30 * (props.public ? 1 : 6)).toISOString(),
-          singleEvents: true,
-          timeZone: 'Europa/Vienna',
-          orderBy: 'startTime'
-        })).data.items?.map(event => ({
-          id: event.id,
-          summary: event.summary,
-          description: event.description ?? null,
-          date: (event.start?.date ?? event.start?.dateTime ?? '').substr(0, 10),
-          start: event.start,
-          end: event.end,
-          calendar: name,
-          visibility: event.visibility ?? 'public',
-          wholeday: !!event.start?.date,
-        } as CalendarEvent)).filter(event => event.summary)
-      )
-  )).flat()
-    .filter(event => !!event) as CalendarEvent[];
+    Object.entries(calendarIds).map(([name, calendarId]) => getEventsFromCalendar(calendarId, name, props.public))
+  ))
+    .flat()
+    .filter(event => !!event);
 
   return allEvents
-    .filter(event => !props.public || event?.visibility === 'public')
-    .map(event => !props.public ? event : ({
-        ...event,
-        summary: event.summary.replace(/\[.*?]/g, ''),
-        description: event.description?.replace(/\[.*?]/g, ''),
-      }))
     .sort((a, b) => getTimeOfEvent(a) - getTimeOfEvent(b))
     .reduce((previous, current) => {
       previous[current!.date] = previous[current!.date] ?? [];
