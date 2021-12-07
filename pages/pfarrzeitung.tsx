3f9f55d56abc20d3685e531522ed125ee1a58986
Manuel Wolkowitsch <e1650767@student.tuwien.ac.at>
2021-12-07 22:20:06 +0100

    Restyle pfarrzeitungen

--- pages/pfarrzeitung.tsx
+++ pages/pfarrzeitung.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import Site from '../components/Site';
 import Link from 'next/link';
+import {CalendarInfo, getCalendarInfo} from '../util/calendar-info';
+
+function Pfarrblatt(props: { calendarInfo: CalendarInfo, href: string, label:string }) {
+  return <Link href={props.href}>
+    <div className={`${props.calendarInfo.className} p-6 rounded-xl cursor-pointer shadow`}>
+      <div className="uppercase mb-4 text-sm">{props.calendarInfo.fullName}</div>
+      <div className="text-3xl font-serif">{props.label}</div>
+    </div>
+  </Link>;
+}
 
 export default function HomePage() {
+  const info = ['emmaus','inzersdorf','neustift'].map((x:any) => getCalendarInfo(x));
   return <Site title="Pfarrzeitungen">
     <div>
       Ausführliche Berichte zum Pfarrleben, Diskussionen zur Weltkirche, Impulse zum Nachdenken und vieles mehr
       finden Sie in den Pfarrzeitungen der Pfarren.
     </div>
-    <div className="grid md:grid-cols-3 gap-4 my-4 h-80 text-3xl font-serif">
-      <Link href="https://tesarekplatz.at/aktuell/emmausbote">
-        <div className="border-4 border-primary1 p-10 rounded-xl cursor-pointer">
-          Der Emmausbote
-        </div>
-      </Link>
-      <Link href="https://www.pfarresanktnikolaus.at/wp/?page_id=89">
-        <div className="border-4 border-primary2 p-10 rounded-xl cursor-pointer">
-          Blickpunkt
-        </div>
-      </Link>
-      <Link href="https://www.erzdioezese-wien.at/pages/pfarren/9233/pfarrblatt">
-        <div className="border-4 border-primary3 p-10 rounded-xl cursor-pointer">
-          Inzersdorf-Neustift News
-        </div>
-      </Link>
+    <div className="grid md:grid-cols-3 gap-4 my-12 h-80">
+      <Pfarrblatt calendarInfo={info[0]} label="Der Emmausbote" href="https://tesarekplatz.at/aktuell/emmausbote"/>
+      <Pfarrblatt calendarInfo={info[1]} label="Blickpunkt" href="https://www.pfarresanktnikolaus.at/wp/?page_id=89"/>
+      <Pfarrblatt calendarInfo={info[2]} label="Inzersdorf-Neustift News" href="https://www.erzdioezese-wien.at/pages/pfarren/9233/pfarrblatt"/>
     </div>
   </Site>
 }
