ae21dfb6b6a368804e39f49d43250ededc61aa71
Manuel <e1650767@student.tuwien.ac.at>
2022-08-11 15:38:52 +0300

    Error notice

--- components/calendar/CalendarCacheNotice.tsx
+++ components/calendar/CalendarCacheNotice.tsx
@@ -3,10 +3,10 @@ import {useCalendarStore} from '../../util/use-calendar-store';
 
 export function CalendarCacheNotice({}) {
   const cached = useCalendarStore(state => state.cache);
-  return cached ? <div className="flex items-center  mb-5">
+  return cached ? <div className="flex items-center  mb-5 p-3 border-2 border-[#f00]">
     <div className="bg-gray-800 rounded-3xl w-5 h-5 text-center text-white text-xl font-bold leading-6 mr-2">
       <div>i</div>
     </div>
-    <div className="italic">Diese Termine wurden zuletzt aktualisiert am {new Date(cached).toLocaleDateString("de-AT")} um {new Date(cached).toLocaleTimeString()}.</div>
+    <div className="italic">Aufgrund eines Systemfehlers werden derzeit nur Termine aus Emmaus angezeigt. Das Problem wird in der Nacht von Donnerstag auf Freitag behoben. Bitte entschuldigen Sie den Fehler!</div>
   </div> : <></>;
 }
