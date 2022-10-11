a3a06e2747097d45a3641fce417798f93ce855e6
Manuel Wolkowitsch <manuel.wolkowitsch@gmail.com>
2022-10-11 18:57:33 +0200

    fix: try solve framer error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
-module.exports = {
-    webpack: config => {
-        config.node = {
-            fs: 'empty',
-            child_process: 'empty',
-            net: 'empty',
-            dns: 'empty',
-            tls: 'empty',
-        };
-        return config;
+const nextConfig = {
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false
+            config.resolve.fallback.tls = false
+            config.resolve.fallback.net = false
+            config.resolve.fallback.child_process = false
+        }
+        return config
     },
-};
+    future: {
+        webpack5: true,
+    },
+    fallback: {
+        fs: false,
+        tls: false,
+        net: false,
+        child_process: false
+    },
+}
+
+module.exports = nextConfig;
