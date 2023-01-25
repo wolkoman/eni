0f978f4450aec09bdfbdd1999111b5c7c5886047
Manuel Wolkowitsch <manuel.wolkowitsch@gmail.com>
2023-01-25 13:32:43 +0100

    show files

--- pages/intern/index.tsx
+++ pages/intern/index.tsx
@@ -7,6 +7,7 @@ import {InternButton} from "../../components/InternButton";
 import {cockpit} from "../../util/cockpit-sdk";
 import CockpitSDK, {Collections} from "cockpit-sdk";
 import Button from "../../components/Button";
+import Link from "next/link";
 
 function AnnouncementsAdmin() {
 
@@ -14,14 +15,18 @@ function AnnouncementsAdmin() {
     const [ann, setAnn] = useState<Collections["announcements"][]>([]);
 
     useEffect(() => {
-        if(!apiKey) return;
-        cockpit.collectionGet('announcements', {token: apiKey, filter: {hidden: false}}).then(({entries}) => setAnn(entries));
+        if (!apiKey) return;
+        cockpit.collectionGet('announcements', {
+            token: apiKey,
+            filter: {hidden: false}
+        }).then(({entries}) => setAnn(entries));
     }, [apiKey])
 
     function hide(id: string) {
         const obj = ann.find(announcement => announcement._id === id)
         const adminCockpit = new CockpitSDK({host: cockpit.host, accessToken: apiKey});
         adminCockpit.collectionSave(('announcements') as any, {...obj, hidden: true});
+        setAnn(rest => rest.filter(a => a._id !== id));
     }
 
     return <>
@@ -29,7 +34,10 @@ function AnnouncementsAdmin() {
         <div className="flex flex-col gap-4">
             {ann.map(announcement => <div className="px-4 py-2 bg-black/[4%] rounded-lg flex flex-col">
                 <div className="my-1 italic">{announcement.mail}</div>
-                <div>{announcement.description}</div>
+                <div className="font-bold my-2">{announcement.description}</div>
+                {announcement.files.map(file => <Link href={"https://api.eni.wien/files-v0/uploads/" + file}>
+                    <div className="underline hover:no-underline">{file}</div>
+                </Link>)}
                 <div className="flex justify-end gap-2">
                     <Button label="Erledigt" onClick={() => hide(announcement._id)}/>
                 </div>
@@ -50,21 +58,23 @@ export default function Intern() {
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
             <InternButton href="/" label="Startseite"/>
             <InternButton href="/termine" label="Termine"/>
-            {(permissions?.[Permission.Reader] || permissions?.[Permission.ReaderPlanning]) && <InternButton href="intern/reader/my" label="Lektor:innen"/>}
+            {(permissions?.[Permission.Reader] || permissions?.[Permission.ReaderPlanning]) &&
+                <InternButton href="intern/reader/my" label="Lektor:innen"/>}
             {permissions?.[Permission.OrganBooking] && <InternButton href="intern/orgel" label="Orgel Buchung"/>}
             {permissions?.[Permission.Editor] && <InternButton href="intern/editor" label="Redaktion"/>}
-            {permissions?.[Permission.LimitedEventEditing] && <InternButton href="intern/limited-event-editing" label="Musik Inzersdorf"/>}
+            {permissions?.[Permission.LimitedEventEditing] &&
+                <InternButton href="intern/limited-event-editing" label="Musik Inzersdorf"/>}
             <InternButton href="https://forms.gle/vCeFKfYwXL7E8ct7A" label="Feedback"/>
             {user?.is_person && <InternButton href="intern/change-password" label="Passwort ändern"/>}
             {user && !user?.is_person && <InternButton href="https://data.eni.wien" label="Cockpit"/>}
             <InternButton onClick={logout} label="Logout"/>
         </div>
         {permissions?.[Permission.Admin] && <>
-          <div className="my-8 text-xl">Administration</div>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
-            <InternButton href="intern/artikel" label="Importer"/>
-            <InternButton href="intern/weekly" label="Wochenmitteilung"/>
-          </div>
+            <div className="my-8 text-xl">Administration</div>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
+                <InternButton href="intern/artikel" label="Importer"/>
+                <InternButton href="intern/weekly" label="Wochenmitteilung"/>
+            </div>
         </>}
         {permissions?.[Permission.Admin] && <AnnouncementsAdmin/>}
     </Site>
