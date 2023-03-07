9614b11d1318db789e3583cf6acbff067d3fe0bb
Manuel Wolkowitsch <manuel.wolkowitsch@gmail.com>
2023-03-07 15:09:57 +0100

    update announcements

--- pages/intern/weekly.tsx
+++ pages/intern/weekly.tsx
@@ -18,6 +18,7 @@ import {EniLoading} from "../../components/Loading";
 import PDFMerger from 'pdf-merger-js/browser';
 import {toast} from "react-toastify";
 import {fetchJson} from "../../util/fetch-util";
+import {getCalendarInfo} from "../../util/calendar-info";
 
 export default function InternArticles() {
     usePermission([Permission.Admin]);
@@ -113,7 +114,6 @@ function AnnouncementsSend() {
             .then(({errorMessage}) => {
                 setState(errorMessage ? 'idle' : 'success');
                 if(errorMessage) toast.error(errorMessage)
-                console.log({errorMessage})
             })
             .catch(() => setState('idle'));
     }
@@ -215,9 +215,9 @@ function AnnouncementsEntries() {
         <div className="flex flex-col gap-4">
             {ann === null && <EniLoading/>}
             {ann?.length === 0 && <div>Es sind keine Ankündigungen eingereicht.</div>}
-            {ann?.map(announcement => <div className="px-6 py-4 bg-black/[2%] rounded-lg flex flex-col">
-                <div className="mb-1 italic">{announcement.mail}</div>
-                <div className="font-bold my-2">{announcement.description}</div>
+            {ann?.map(announcement => <div className="px-6 py-4 bg-black/[2%] rounded-lg flex flex-col" key={announcement._id}>
+                <div className="mb-1 font-bold">{getCalendarInfo(announcement.parish as any).fullName}: {announcement.byName}</div>
+                <div className="mb-2">{announcement.description}</div>
                 {announcement.files.map(file => <Link href={"https://api.eni.wien/files-v0/uploads/" + file}>
                     <div className="underline hover:no-underline">{file}</div>
                 </Link>)}
