f6b7d951c0ae26e4110f9e4acfc053d017bbda9c
Manuel Wolkowitsch <manuel@magic.dev>
2024-01-26 09:48:41 +0100

    Make refreshing token non static

--- app/termine/google/expired/route.ts
+++ app/termine/google/expired/route.ts
@@ -1,6 +1,8 @@
 import {NextResponse} from "next/server";
 import {get} from "@vercel/edge-config";
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: Request) {
 
   const google_config: any = await get('google_config');
