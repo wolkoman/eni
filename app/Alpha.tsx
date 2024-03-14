46a2a6fd04a2c5e3aa36c7483b9172b8b42c5f1a
Manuel Wolkowitsch <manuel@magic.dev>
2024-03-14 18:31:47 +0100

    add alpha

---
+++ app/Alpha.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import {SectionHeader} from "../components/SectionHeader";
+import Link from "next/link";
+import {loadCachedEvents} from "@/domain/events/EventsLoader";
+import {EventLoadAccess} from "@/domain/events/EventLoadOptions";
+import {site} from "@/app/(shared)/Instance";
+import Site from "../components/Site";
+import TopBar from "../components/TopBar";
+import {EniHero} from "../components/EniHero";
+import Responsive from "../components/Responsive";
+import {TodayAndTomorrow} from "../components/TodayAndTomorrow";
+import {ComingUp} from "../components/calendar/ComingUp";
+import {EniSections} from "@/app/EniSections";
+import {Instagram} from "../components/Instagram";
+import {fetchCachedInstagramFeed} from "@/app/(shared)/Instagram";
+import {EniInformation} from "@/app/EniInformation";
+import {EmmausBranding} from "../components/EmmausBranding";
+import {WorshipNotice} from "../components/WorshipNotice";
+import EmmausSections, {Articles} from "@/app/EmmausSections";
+import {fetchArticles} from "@/app/(shared)/Articles";
+import {fetchEmmausSites} from "@/app/(shared)/Sites";
+import {fetchEmmausbote} from "@/app/(shared)/Weekly";
+import {PrayerWall} from "@/app/(emmaus-only)/gebetswand/PrayerWall";
+import * as React from "react";
+import {ReactNode, useRef, useState} from "react";
+
+function S(props: {title: string, children: ReactNode}){
+  return <div className="my-2">
+    <div className="font-semibold">{props.title}</div>
+    <div>{props.children}</div>
+  </div>
+}
+
+export function Alpha() {
+
+  const [play, setPlay] = useState(false)
+  const [controls, setControls] = useState(false)
+  const ref = useRef<HTMLVideoElement>(null)
+
+  return <div className="my-8">
+    <SectionHeader>Alpha Kurs</SectionHeader>
+    <div className="flex">
+      <div className="flex items-center relative">
+        {!play && <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+        onClick={() => {
+          ref.current?.play()
+          setPlay(true);
+          setTimeout(() => setControls(true), 5000)
+        }}>
+          <svg className="w-12 h-12 hover:scale-110 transition cursor-pointer" width="80" height="80" viewBox="0 0 80 80">
+            <circle cx="40" cy="40" r="40" fill="#333333" />
+            <path d="M55 40L30 55V25L55 40Z" fill="#FFFFFF" />
+          </svg>
+        </div>}
+        <video controls={controls} ref={ref}
+               className={"rounded-lg overflow-hidden border border-black/20 aspect-video w-full relative " + (play ? "" : "opacity-50")}>
+          <source src="https://data.eni.wien/storage/uploads/2024/03/14/eine_einladung-540p_uid_65f2d34882b3e.mp4"
+                  type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="p-4">
+        <div className="font-2xl font-semibold">Machen Sie mit!</div>
+        <S title="Abendeinheiten (19:00-21:30 Uhr, mit Abendessen)">
+          9., 16., 23. und 30. April, der 7., 14., 21. und 28. Mai,
+          sowie der 11. und 18. Juni 2024
+        </S>
+        <S title="Tageseinheit (ganzer Tag)">8. Juni 2024</S>
+        <S title="Teilnahme">kostenlos, aber Anmeldung erforderlich</S>
+        <S title="Anmeldung">
+          Pastoralassistent Pedro
+          Widler und sein Team werden den Alphakurs begleiten<br/>
+          Infos und Anmeldung: E-Mail: <Link href="mailto:pass@eni.wien">pass@eni.wien</Link><br/>
+          Telefon: 0676/559 33 48</S>
+      </div>
+    </div>
+  </div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,71 @@
-import Site from '../components/Site';
-import TopBar from '../components/TopBar';
-import {Instagram} from "../components/Instagram";
-import {EmmausBranding} from "../components/EmmausBranding";
-import EmmausSections, {Articles} from "./EmmausSections";
-import {ComingUp} from "../components/calendar/ComingUp";
+import {loadCachedEvents} from "@/domain/events/EventsLoader";
+import {EventLoadAccess} from "@/domain/events/EventLoadOptions";
+import {site} from "@/app/(shared)/Instance";
+import Site from "../components/Site";
+import TopBar from "../components/TopBar";
 import {EniHero} from "../components/EniHero";
-import {WorshipNotice} from "../components/WorshipNotice";
 import Responsive from "../components/Responsive";
-import {PrayerWall} from "./(emmaus-only)/gebetswand/PrayerWall";
-import {loadCachedEvents} from "./(domain)/events/EventsLoader";
-import {EventLoadAccess} from "./(domain)/events/EventLoadOptions";
-import {site} from "./(shared)/Instance";
-import {fetchCachedInstagramFeed} from "./(shared)/Instagram";
-import {fetchEmmausSites} from "./(shared)/Sites";
-import {EniSections} from "./EniSections";
-import {fetchArticles} from "./(shared)/Articles";
-import {fetchEmmausbote} from "./(shared)/Weekly";
-import {EniInformation} from "./EniInformation";
 import {TodayAndTomorrow} from "../components/TodayAndTomorrow";
+import {ComingUp} from "../components/calendar/ComingUp";
+import {EniSections} from "@/app/EniSections";
+import {Instagram} from "../components/Instagram";
+import {fetchCachedInstagramFeed} from "@/app/(shared)/Instagram";
+import {EniInformation} from "@/app/EniInformation";
+import {EmmausBranding} from "../components/EmmausBranding";
+import {WorshipNotice} from "../components/WorshipNotice";
+import EmmausSections, {Articles} from "@/app/EmmausSections";
+import {fetchArticles} from "@/app/(shared)/Articles";
+import {fetchEmmausSites} from "@/app/(shared)/Sites";
+import {fetchEmmausbote} from "@/app/(shared)/Weekly";
+import {PrayerWall} from "@/app/(emmaus-only)/gebetswand/PrayerWall";
+import * as React from "react";
+import {Alpha} from "@/app/Alpha";
 
 export const revalidate = 300
+
 
 export default async function HomePage() {
   const eventsObject = await loadCachedEvents({access: EventLoadAccess.PUBLIC})
   return site(async () => <Site
-    responsive={false} navbar={false}
-    description="Drei Pfarren im Wiener Dekanat 23"
-    keywords={["Katholisch", "Pfarre", "Glaube", "Gemeinschaft"]}>
-    <TopBar frontpage={true}/>
-    <EniHero/>
-    <Responsive size="md">
-      <TodayAndTomorrow eventsObject={eventsObject}/>
-      <ComingUp eventsObject={eventsObject}/>
-      <EniSections/>
-      <Instagram items={await fetchCachedInstagramFeed()}/>
-      <EniInformation/>
-    </Responsive>
-  </Site>, async () => <Site
-    responsive={false} navbar={false}
-    description="Eine katholische Pfarre im Wiener Dekanat 23"
-    keywords={["Katholisch", "Pfarre", "Glaube", "Gemeinschaft"]}>
-    <div className="md:sticky inset-0 w-full">
+      responsive={false} navbar={false}
+      description="Drei Pfarren im Wiener Dekanat 23"
+      keywords={["Katholisch", "Pfarre", "Glaube", "Gemeinschaft"]}>
       <TopBar frontpage={true}/>
-      <EmmausBranding eventsObject={eventsObject}/>
-    </div>
-    <WorshipNotice worshipEvents={eventsObject.events.filter(event => event.summary === "Worship")}/>
-    <div className="relative z-10 bg-white">
-    <Responsive>
-      <div className="grid lg:grid-cols-2 gap-8 my-8">
+      <EniHero/>
+      <Responsive size="md">
         <TodayAndTomorrow eventsObject={eventsObject}/>
-        <Articles items={await fetchArticles()}/>
-      </div>
-    </Responsive>
-      <EmmausSections
-        sites={await fetchEmmausSites()}
-        emmausbote={await fetchEmmausbote()}
-      />
-      <Responsive>
         <ComingUp eventsObject={eventsObject}/>
-        <PrayerWall/>
+        <EniSections/>
         <Instagram items={await fetchCachedInstagramFeed()}/>
+        <EniInformation/>
       </Responsive>
-    </div>
-  </Site>)();
+    </Site>, async () => <Site
+      responsive={false} navbar={false}
+      description="Eine katholische Pfarre im Wiener Dekanat 23"
+      keywords={["Katholisch", "Pfarre", "Glaube", "Gemeinschaft"]}>
+      <div className="md:sticky inset-0 w-full">
+        <TopBar frontpage={true}/>
+        <EmmausBranding eventsObject={eventsObject}/>
+      </div>
+      <WorshipNotice worshipEvents={eventsObject.events.filter(event => event.summary === "Worship")}/>
+      <div className="relative z-10 bg-white">
+        <Responsive>
+          <div className="grid lg:grid-cols-2 gap-8 my-8">
+            <TodayAndTomorrow eventsObject={eventsObject}/>
+            <Articles items={await fetchArticles()}/>
+          </div>
+          {new Date() < new Date("2024-04-09") && <Alpha/>}
+        </Responsive>
+        <EmmausSections
+          sites={await fetchEmmausSites()}
+          emmausbote={await fetchEmmausbote()}
+        />
+        <Responsive>
+          <ComingUp eventsObject={eventsObject}/>
+          <PrayerWall/>
+          <Instagram items={await fetchCachedInstagramFeed()}/>
+        </Responsive>
+      </div>
+    </Site>
+  )();
 }
