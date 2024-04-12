5430ad1ba608af7a32a255be8f493653bac97795
Manuel Wolkowitsch <manuel@magic.dev>
2024-04-12 12:20:59 +0200

    override config

--- app/(shared)/GoogleAuthClient.ts
+++ app/(shared)/GoogleAuthClient.ts
@@ -3,12 +3,14 @@ import {google} from "googleapis";
 import {get} from "@vercel/edge-config";
 
 export async function getGoogleAuthClient() {
-    const google_config: any = await get('google_config');
-    const oauth2Client = new google.auth.OAuth2(
-        process.env.GOOGLE_CLIENT_ID,
-        process.env.GOOGLE_CLIENT_KEY,
-        "https://eni.wien/termine/google/callback"
-    );
-    oauth2Client.setCredentials(google_config);
-    return oauth2Client;
+  const google_config: any = process.env.GOOGLE_STORAGE_OVERRIDE
+    ? process.env.GOOGLE_STORAGE_OVERRIDE
+    : await get('google_configs');
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_KEY,
+    "https://eni.wien/termine/google/callback"
+  );
+  oauth2Client.setCredentials(google_config);
+  return oauth2Client;
 }
