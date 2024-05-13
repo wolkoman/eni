e98cd3ef8ef2962c064ba6241dad250d7f98d84f
Manuel Wolkowitsch <manuel@magic.dev>
2024-05-13 09:03:47 +0200

    fix(events): fix event revalidation

--- app/termine/page.tsx
+++ app/termine/page.tsx
@@ -3,6 +3,8 @@ import {loadCachedLiturgyData} from "../../pages/api/liturgy";
 import {loadCachedEvents} from "../(domain)/events/EventsLoader";
 import {EventLoadAccess} from "../(domain)/events/EventLoadOptions";
 
+export const revalidate = 300;
+
 export default async function () {
   const liturgy = await loadCachedLiturgyData()
   const eventsObject = await loadCachedEvents({access: EventLoadAccess.PUBLIC})
